74ce12ffc2709632e3ad4711c7d1849031ee7132
Ismail ech-chaouny <chaouniismail4@gmail.com>
2024-01-22 17:33:30 +0100

    Final Commit V1

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -11,81 +11,54 @@ const Hero = forwardRef(function index(props, ref) {
   const sm = useTransform(scrollYProgress, [0, 1], [90, -90]);
   const md = useTransform(scrollYProgress, [0, 1], [110, -110]);
   return (
-    <>
-      <div
-        ref={ref}
-        className="flex flex-col justify-center items-center text-center pt-12"
+    <div
+      ref={ref}
+      className="flex flex-col justify-center items-center text-center pt-12"
+    >
+      <motion.div
+        initial={{ scale: 0.9, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ delay: 3, duration: 1 }}
       >
-        <motion.div
-          initial={{ scale: 0.9, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          transition={{ delay: 3, duration: 1 }}
-        >
-          <IsmaelSmile className="w-60" />
-        </motion.div>
-        <h1
-          className={`font-black ${
-            props.isDarkMode ? "text-white-300" : "text-secondary"
-          } duration font-generalSans xs:text-[60px] text-[32px] mt-6 tracking-tight leading-tight`}
-        >
-          <div className="overflow-hidden">
-            <motion.div
-              initial={{ y: 200 }}
-              animate={{ y: 0 }}
-              transition={{ delay: 2.3, duration: 1, ease: [0.37, 0, 0.63, 1] }}
-            >
-              Hello, I'm
-            </motion.div>
-          </div>
-          <div className="overflow-hidden">
-            <motion.div
-              initial={{ y: 200 }}
-              animate={{ y: 0 }}
-              transition={{ delay: 2.3, duration: 1, ease: [0.37, 0, 0.63, 1] }}
-            >
-              Ismael Ech-chaouny
-            </motion.div>
-          </div>
-        </h1>
-        <div className=" overflow-hidden">
-          <motion.p
-            initial={{ y: 200 }}
-            animate={{ y: 0 }}
-            transition={{ delay: 2.5 }}
-            className={`mt-5 xs:text-[25px] text-[18px] max-w-[500px] font-generalSans ${
-              props.isDarkMode ? "text-white-300" : "text-secondary"
-            } font-medium duration`}
-          >
-            A passionate Front-end Developer devoted to the art of coding. 👋
-          </motion.p>
-        </div>
-      </div>
-      <div className="absolute sm:right-0 center-0 bottom-0 mb-8 sm:mr-12 sm:mb-16">
-        <a
-          href="#about"
-          className="flex flex-col justify-center items-center gap-2"
-        >
+        <IsmaelSmile className="w-60" />
+      </motion.div>
+      <h1
+        className={`font-black ${
+          props.isDarkMode ? "text-white-300" : "text-secondary"
+        } duration font-generalSans xs:text-[60px] text-[32px] mt-6 tracking-tight leading-tight`}
+      >
+        <div className="overflow-hidden">
           <motion.div
             initial={{ y: 200 }}
             animate={{ y: 0 }}
-            transition={{ delay: 2.3, duration: 1 }}
-            className="w-[25px] h-[47px]
-          rounded-3xl border-[3px] border-secondary
-          flex justify-center items-start p-1.5"
+            transition={{ delay: 2.3, duration: 1, ease: [0.37, 0, 0.63, 1] }}
           >
-            <motion.div
-              animate={{ y: [0, 24, 0] }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-1.5 h-1.5 rounded-full bg-secondary mb-1"
-            />
+            Hello, I'm
           </motion.div>
-        </a>
+        </div>
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ y: 200 }}
+            animate={{ y: 0 }}
+            transition={{ delay: 2.3, duration: 1, ease: [0.37, 0, 0.63, 1] }}
+          >
+            Ismael Ech-chaouny
+          </motion.div>
+        </div>
+      </h1>
+      <div className=" overflow-hidden">
+        <motion.p
+          initial={{ y: 200 }}
+          animate={{ y: 0 }}
+          transition={{ delay: 2.5 }}
+          className={`mt-5 xs:text-[25px] text-[18px] max-w-[500px] font-generalSans ${
+            props.isDarkMode ? "text-white-300" : "text-secondary"
+          } font-medium duration`}
+        >
+          A passionate Front-end Developer devoted to the art of coding. 👋
+        </motion.p>
       </div>
-    </>
+    </div>
   );
 });
 
